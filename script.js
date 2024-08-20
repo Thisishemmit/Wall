a151ed0e45bed1636152eb25dd862d1ea1162f6b
Thisishemmit <mkaroumi123@gmail.com>
2024-08-20 02:10:26 +0100

    sak waha

--- script.js
+++ script.js
@@ -19,141 +19,116 @@ precision mediump float;
 
 uniform vec2 resolution;
 uniform float time;
-uniform sampler2D noise2;
 
-#define iChannel0 noise2
 #define iTime time
-#define STEP 256
-#define EPS .001
 
+float det = 0.001, t, boxhit;
+vec3 adv, boxp;
 
-// from various shader by iq
-
-float smin( float a, float b, float k )
+float hash(vec2 p)
 {
-    float h = clamp( 0.5+0.5*(b-a)/k, 0.0, 1.0 );
-    return mix( b, a, h ) - k*h*(1.0-h);
+    vec3 p3 = fract(vec3(p.xyx) * 0.1031);
+    p3 += dot(p3, p3.yzx + 33.33);
+    return fract((p3.x + p3.y) * p3.z);
 }
 
-const mat2 m = mat2(.8,.6,-.6,.8);
-
-float noise( in vec2 x )
+mat2 rot(float a)
 {
-	return sin(1.5*x.x)*sin(1.5*x.y);
+    float s = sin(a), c = cos(a);
+    return mat2(c, s, -s, c);
 }
 
-float fbm6( vec2 p )
+vec3 path(float t)
 {
-    float f = 0.0;
-    f += 0.500000*(0.5+0.5*noise( p )); p = m*p*2.02;
-    f += 0.250000*(0.5+0.5*noise( p )); p = m*p*2.03;
-    f += 0.125000*(0.5+0.5*noise( p )); p = m*p*2.01;
-    f += 0.062500*(0.5+0.5*noise( p )); p = m*p*2.04;
-    //f += 0.031250*(0.5+0.5*noise( p )); p = m*p*2.01;
-    f += 0.015625*(0.5+0.5*noise( p ));
-    return f/0.96875;
+    vec3 p = vec3(vec2(sin(t * 0.1), cos(t * 0.05)) * 10.0, t);
+    p.x += smoothstep(0.0, 0.5, abs(0.5 - fract(t * 0.02))) * 10.0;
+    return p;
 }
 
-
-mat2 getRot(float a)
+float fractal(vec2 p)
 {
-    float sa = sin(a), ca = cos(a);
-    return mat2(ca,-sa,sa,ca);
-}
-
-
-vec3 _position;
-
-float sphere(vec3 center, float radius)
-{
-    return distance(_position,center) - radius;
-}
-
-float hozPlane(float height)
-{
-    return distance(_position.y,height);
-}
-
-float swingPlane(float height)
-{
-    vec3 pos = _position + vec3(0.,0.,iTime * 2.5);
-    float def =  fbm6(pos.xz * .25) * 1.;
-
-    float way = pow(abs(pos.x) * 34. ,2.5) *.0000125;
-    def *= way;
-
-    float ch = height + def;
-    return max(pos.y - ch,0.);
-}
-
-float map(vec3 pos)
-{
-    _position = pos;
-
-    float dist;
-    dist = swingPlane(0.);
-
-    float sminFactor = 5.25;
-    dist = smin(dist,sphere(vec3(0.,-15.,80.),45.),sminFactor);
-    return dist;
-}
-
-
-vec3 getNormal(vec3 pos)
-{
-    vec3 nor = vec3(0.);
-    vec3 vv = vec3(0.,1.,-1.)*.01;
-    nor.x = map(pos + vv.zxx) - map(pos + vv.yxx);
-    nor.y = map(pos + vv.xzx) - map(pos + vv.xyx);
-    nor.z = map(pos + vv.xxz) - map(pos + vv.xxy);
-    nor /= 2.;
-    return normalize(nor);
-}
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-	vec2 uv = (fragCoord.xy-.5*resolution.xy)*2./resolution.y;
-
-    vec3 rayOrigin = vec3(uv + vec2(0.,6.), -1. );
-
-    vec3 rayDir = normalize(vec3(uv , 1.));
-
-   	rayDir.zy = getRot(.05) * rayDir.zy;
-   	rayDir.xy = getRot(.075) * rayDir.xy;
-
-    vec3 position = rayOrigin;
-
-
-    float curDist;
-    int nbStep = 0;
-
-    for(; nbStep < STEP;++nbStep)
+    p = abs(5.0 - mod(p * 0.2, 10.0)) - 5.0;
+    float ot = 1000.0;
+    for (int i = 0; i < 7; i++)
     {
-        curDist = map(position + (texture(iChannel0, position.xz) - .5).xyz * .005);
-
-        if(curDist < EPS)
-            break;
-        position += rayDir * curDist * .5;
+        p = abs(p) / clamp(p.x * p.y, 0.25, 2.0) - 1.0;
+        if (i > 0) ot = min(ot, abs(p.x) + 0.7 * fract(abs(p.y) * 0.05 + t * 0.05 + float(i) * 0.3));
     }
-
-    float f;
-
-    //sound = sin(iTime) * .5 + .5;
-
-    float dist = distance(rayOrigin,position);
-    f = dist /(98.);
-    f = float(nbStep) / float(STEP);
-
-    f *= .8;
-    vec3 col = vec3(f);
-
-
-    //float shouldColor = 1.- step(f,threshold);
-    //col = mix(col,vec3(1.,0.,0.) ,shouldColor);
-
-    fragColor = vec4(col,1.0);
+    ot = exp(-10.0 * ot);
+    return ot;
 }
+
+float box(vec3 p, vec3 l)
+{
+    vec3 c = abs(p) - l;
+    return length(max(vec3(0.0), c)) + min(0.0, max(c.x, max(c.y, c.z)));
+}
+
+float de(vec3 p)
+{
+    boxhit = 0.0;
+    vec3 p2 = p - adv;
+    p2.xz *= rot(t * 0.2);
+    p2.xy *= rot(t * 0.1);
+    p2.yz *= rot(t * 0.15);
+    float b = box(p2, vec3(1.0));
+    p.xy -= path(p.z).xy;
+    float s = sign(p.y);
+    p.y = -abs(p.y) - 3.0;
+    p.z = mod(p.z, 20.0) - 10.0;
+    for (int i = 0; i < 5; i++)
+    {
+        p = abs(p) - 1.0;
+        p.xz *= rot(radians(s * -45.0));
+        p.yz *= rot(radians(90.0));
+    }
+    float f = -box(p, vec3(5.0, 5.0, 10.0));
+    float d = min(f, b);
+    if (d == b) boxp = p2, boxhit = 1.0;
+    return d * 0.7;
+}
+
+vec3 march(vec3 from, vec3 dir)
+{
+    vec3 p, n, g = vec3(0.0);
+    float d, td = 0.0;
+    for (int i = 0; i < 80; i++)
+    {
+        p = from + td * dir;
+        d = de(p) * (1.0 - hash(gl_FragCoord.xy + t) * 0.3);
+        if (d < det && boxhit < 0.5) break;
+        td += max(det, abs(d));
+        float f = fractal(p.xy) + fractal(p.xz) + fractal(p.yz);
+        float b = fractal(boxp.xy) + fractal(boxp.xz) + fractal(boxp.yz);
+        vec3 colf = vec3(f);
+        vec3 colb = vec3(b + 0.1, b * b + 0.05, 0.0);
+        g += colf / (3.0 + d * d * 2.0) * exp(-0.0015 * td * td) * step(5.0, td) / 2.0 * (1.0 - boxhit);
+        g += colb / (10.0 + d * d * 20.0) * boxhit * 0.5;
+    }
+    return g;
+}
+
+mat3 lookat(vec3 dir, vec3 up)
+{
+    dir = normalize(dir);
+    vec3 rt = normalize(cross(dir, normalize(up)));
+    return mat3(rt, cross(rt, dir), dir);
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec2 uv = (fragCoord - resolution.xy * .5)*2. / resolution.y;
+    t = iTime * 7.0;
+    vec3 from = path(t);
+    adv = path(t + 6.0 + sin(t * 0.1) * 3.0);
+    vec3 dir = normalize(vec3(uv, 0.7));
+    dir = lookat(adv - from, vec3(0.0, 1.0, 0.0)) * dir;
+    vec3 col = march(from, dir);
+    fragColor = vec4(col, 1.0);
+}
+
 out vec4 frag;
+
 void main() {
 	vec4 fragment_color;
 	mainImage(fragment_color, gl_FragCoord.xy);
@@ -273,13 +248,13 @@ async function render() {
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
     gl.uniform1f(timeUniformLocation, time);
 
-    gl.activeTexture(gl.TEXTURE0);
+    /* gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, noiseTexture);
     gl.uniform1i(noise2UniformLocation, 0);
 
     gl.activeTexture(gl.TEXTURE1);
     gl.bindTexture(gl.TEXTURE_2D, fontTexture);
-    gl.uniform1i(font1UniformLocation, 1);
+    gl.uniform1i(font1UniformLocation, 1); */
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     log.textContent = `Time: ${time}, Frame: ${frameNumber}`;
